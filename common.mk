4b1e7184f229ac77272b99cb8b2cb132eeaefd93
Rohan Hasabe (Hasaber8) <rohanhasabe8@gmail.com>
2020-05-11 02:14:10 +0200

    sdm660-common: libperfmgr: Rename pixel to motorola
    * Lets make it our own xD
    
    Signed-off-by: Rohan Hasabe (Hasaber8) <rohanhasabe8@gmail.com>
    Change-Id: I0d855c675a422bf2144c92b2cfff826472a4012a

--- common.mk
+++ common.mk
@@ -310,7 +310,7 @@ PRODUCT_PACKAGES += \
 
 # Pixel Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.pixel-libperfmgr
+    android.hardware.power@1.3-service.motorola-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
